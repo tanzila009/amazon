0889c2329f14ea9a26b23189de5f15e0551a5e0b
tanzila009 <tanzilakhusanovaa@gmail.com>
2024-12-17 21:38:29 +0500

    project

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M tanzila
+git remote add my_origin https://github.com/tanzila009/uzum_market.git
+git push -u my_origin tanzila
+"""
